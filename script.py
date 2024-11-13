2040a2eb593c5580b63d70d830599ab1274579ca
Joel Broberg Olsson <joelbrobergolsson@gmail.com>
2024-11-13 20:55:52 +0100

    Add dict of names

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 def fileopener(file):
     with open(file, "r") as file:
         names = file.readlines()
-        name_list = [name.strip("\n") for name in names]
-        print(name_list)
+        name_list = [name.split() for name in names]
+        name_dict = {}
+        for fl in name_list:
+            name_dict[fl[0]] = fl[1]
+        print(name_dict)
+
 
 fileopener("data.txt")
